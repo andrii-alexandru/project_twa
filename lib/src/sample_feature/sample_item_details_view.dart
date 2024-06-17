76b0914a712d371a71a704bb60a745f590c945e2
petru <p37ruu@gmail.com>
2024-06-17 14:00:21 +0300

    show 5 images in carousel

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class SampleItemDetailsView extends StatefulWidget {
   final String breedName;
@@ -10,34 +11,29 @@ class SampleItemDetailsView extends StatefulWidget {
   static const routeName = '/sample_item';
 
   @override
-  _SampleItemDetailsViewState createState() => _SampleItemDetailsViewState();
+  SampleItemDetailsViewState createState() => SampleItemDetailsViewState();
 }
 
-class _SampleItemDetailsViewState extends State<SampleItemDetailsView> {
-  late Future<String> futureImageUrl;
+class SampleItemDetailsViewState extends State<SampleItemDetailsView> {
+  late Future<List<String>> futureImageUrls;
 
   @override
   void initState() {
     super.initState();
-    futureImageUrl = fetchBreedImage(widget.breedName);
+    futureImageUrls = fetchBreedImages(widget.breedName, 5);
   }
 
-  Future<String> fetchBreedImage(String breed) async {
-    final response = await http
-        .get(Uri.parse('https://dog.ceo/api/breed/$breed/images/random'));
+  Future<List<String>> fetchBreedImages(String breed, int count) async {
+    final response = await http.get(
+      Uri.parse('https://dog.ceo/api/breed/$breed/images/random/$count'),
+    );
 
     if (response.statusCode == 200) {
-      final String imageUrl = json.decode(response.body)['message'];
-      return imageUrl;
+      final List<String> imageUrls = List<String>.from(json.decode(response.body)['message']);
+      return imageUrls;
     } else {
-      throw Exception('Failed to load breed image');
+      throw Exception('Failed to load breed images');
     }
-  }
-
-  void refetchImage() {
-    setState(() {
-      futureImageUrl = fetchBreedImage(widget.breedName);
-    });
   }
 
   @override
@@ -46,41 +42,56 @@ class _SampleItemDetailsViewState extends State<SampleItemDetailsView> {
       appBar: AppBar(
         title: Text(widget.breedName.toUpperCase()),
       ),
-      body: FutureBuilder<String>(
-        future: futureImageUrl,
+      body: FutureBuilder<List<String>>(
+        future: futureImageUrls,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (snapshot.hasData) {
-            final imageUrl = snapshot.data!;
+            final imageUrls = snapshot.data!;
             return Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // resizing image
-                  Container(
-                    width: 300,
-                    height: 300,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(imageUrl),
-                          fit: BoxFit
-                              .fill
-                          ),
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      autoPlay: false,
+                      enlargeCenterPage: true,
+                      viewportFraction: 0.8,
+                      //aspectRatio: 1.0,
+                      //autoPlayInterval: Duration(seconds: 3),
                     ),
+                    items: imageUrls.map((url) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                            width: 300, // Set width to 300 pixels
+                            height: 300, // Set height to 300 pixels
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(url),
+                                fit: BoxFit.fill, // will fill the widthxheight box; may distort images
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
                   ),
                   const SizedBox(height: 20),
                   ElevatedButton(
-                    onPressed: refetchImage,
-                    child: const Text('Fetch New Image'),
+                    onPressed: () => setState(() {
+                      futureImageUrls = fetchBreedImages(widget.breedName, 5);
+                    }),
+                    child: const Text('Fetch New Images'),
                   ),
                 ],
               ),
             );
           } else {
-            return const Center(child: Text('No image found'));
+            return const Center(child: Text('No images found'));
           }
         },
       ),
